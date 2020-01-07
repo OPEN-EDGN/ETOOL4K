076147c3a655cab00a041abf40c8509c5ab315d6
Noob <ExplodingFKL@gmail.com>
2020-01-07 14:05:33 +0800

    # 临时保存

--- logger4k/src/test/java/tech/openEdgn/logger4k/DateFormatTest.kt
+++ logger4k/src/test/java/tech/openEdgn/logger4k/DateFormatTest.kt
@@ -5,7 +5,7 @@ import java.lang.RuntimeException
 
 
 internal class DateFormatTest{
-    val logger = getLogger()
+    private val logger = getLogger()
     @Test
     fun test() {
         logger.error("asasas")
